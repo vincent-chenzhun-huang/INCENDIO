d300a7e0925760d571a1739aeb020cef3ea47194
Vincent Huang <39947361+VincentCloud@users.noreply.github.com>
2020-03-04 15:02:10 -0500

    Update User.ts and index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import { User } from './models/User'
+import axios from 'axios'
 
-const user = new User({ name: 'myname', age: 20})
-
-user.on("change", ()=>{
-    console.log('hello')
+axios.post('http://localhost:3000/users', {
+    name: 'name',
+    age: 20
 })
-
-user.on('change', ()=>{
-    console.log('hello2')
-})
-
-user.trigger('change')
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,8 @@
+import axios, { AxiosResponse } from 'axios'
+
+
 interface UserProps {
+    id?: number
     name?: string
     age?: number
 }
@@ -46,6 +50,13 @@ export class User {
         handlers.forEach(callback => {
             callback()
         })
+    }
+
+    fetch(): void {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`)
+          .then((response: AxiosResponse): void => {
+              this.set(response.data)
+          })
     }
 
 }
